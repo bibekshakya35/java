13f3f9d6f3abec9815b8767a455f289719c90698
Bibek Shakya <bibekshakyanp@gmail.com>
2018-03-19 21:28:59 +0545

    initial commit 1

--- rxjava/src/main/java/rxjava/App.java
+++ rxjava/src/main/java/rxjava/App.java
@@ -1,4 +1,11 @@
 package rxjava;
-
+import io.reactivex.*;
 public class App {
+    public String getGreeting(){
+        return "Hello World";
+    }
+    public static void main(String[] args){
+        Observable.just("Hello World")
+                .subscribe(line->System.out.println(line));
+    }
 }
